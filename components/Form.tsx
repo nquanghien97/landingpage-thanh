1de0cab246143282203237907f207cf9f94ee0da
nquanghien97 <nquanghien97@gmail.com>
2024-09-27 00:10:57 +0700

    implement: send form

--- components/Form.tsx
+++ components/Form.tsx
@@ -22,9 +22,9 @@ const listRadio = [
 function Form() {
   const [checked, setChecked] = useState(0);
   const [formValue, setFormValue] = useState({
-    fullName: '',
-    phoneNumber: '',
-    address: ''
+    'Họ tên': '',
+    'Số điện thoại': '',
+    'Địa chỉ': ''
   })
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
@@ -34,22 +34,26 @@ function Form() {
     });
   }
   
-  const onSubmit = (e: FormEvent<HTMLFormElement>) => {
+  const onSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    const submitForm = {
-      product: listRadio[checked].label,
-      ...formValue
-    }
-    console.log(submitForm)
+    const formData = new FormData(e.currentTarget)
+    formData.append('Số lượng', listRadio[checked].label)
+    await fetch("https://script.google.com/macros/s/AKfycbws-zJPI19r7EtE8sIk6Yopsxcbn_DWxeLvtWpRbxh7SaOPCOP27j8o3mGzU7cjoLQg/exec",
+      {
+        method: "POST",
+        body: formData,
+        mode: 'no-cors'
+      }
+    )
   }
   return (
     <div className="mb-4">
       <h3 className="uppercase text-[#853f05] text-xl font-bold px-4 text-center bg-text mb-4">Đăng ký để nhận ưu đãi tại đây!</h3>
       <form onSubmit={onSubmit}>
         <div className="flex flex-col gap-2 mb-2 px-4 m-auto">
-          <input type="text" placeholder="Họ và tên" className="px-4 py-1 border border-[#a40202] rounded-xl" name="fullName" onChange={handleChange} />
-          <input type="text" placeholder="Số điện thoại" className="px-4 py-1 border border-[#a40202] rounded-xl" name="phoneNumber" onChange={handleChange} />
-          <input type="text" placeholder="Địa chỉ" className="px-4 py-1 border border-[#a40202] rounded-xl" name="address" onChange={handleChange} />
+          <input type="text" placeholder="Họ và tên" className="px-4 py-1 border border-[#a40202] rounded-xl" name="Họ tên" onChange={handleChange} />
+          <input type="text" placeholder="Số điện thoại" className="px-4 py-1 border border-[#a40202] rounded-xl" name="Số điện thoại" onChange={handleChange} />
+          <input type="text" placeholder="Địa chỉ" className="px-4 py-1 border border-[#a40202] rounded-xl" name="Địa chỉ" onChange={handleChange} />
           <div className="border border-[#a40202] p-2 rounded-xl bg-white">
             {listRadio.map((item, index) => (
               <div key={item.id}>
